71a53cd39925d9e380778da06cbd9bc115c723b8
Serkan <serkanerip@gmail.com>
2017-03-23 17:23:31 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,21 +2,9 @@ from keje_framework import Keje
 from router import Router
 import signal
 import sys
-import re
 
 kf = Keje()
 router = Router(kf)
-
-'''def router(request, client_sock):
-	if(request['url'] == "/"):
-		kf.response(client_sock, 'hello')
-	elif(request['url'] == '/users'):
-		kf.response(client_sock, 'users')
-	elif( re.match( '/user/[0-9]+', request['url'] ) ):
-		kf.response(client_sock, 'user')
-	else:
-		kf.response(client_sock, status_code=404)
-'''
 
 kf.router = router.router
 kf.start()
